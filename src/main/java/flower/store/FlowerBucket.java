a1abdd35d85dbbc3ac88c48c7358e5faeaf3b4f5
gashchukk <145265923+gashchukk@users.noreply.github.com>
2023-12-23 23:40:22 +0200

    Update FlowerBucket.java

--- src/main/java/flower/store/FlowerBucket.java
+++ src/main/java/flower/store/FlowerBucket.java
@@ -2,16 +2,19 @@ package flower.store;
 
 import java.util.ArrayList;
 import java.util.List;
-public class FlowerBucket  extends Item{
+
+public class FlowerBucket extends Item {
     private List<FlowerPack> flowerPacks = new ArrayList<>();
-public double getPrice(){
-    double price =0;
-    for(FlowerPack flowerPack : flowerPacks){
-        price += flowerPack.getPrice();
+
+    public double getPrice() {
+        double price = 0;
+        for (FlowerPack flowerPack : flowerPacks) {
+            price += flowerPack.getPrice();
+        }
+        return price;
     }
-    return price;
-}
-    public void addFlowerPack(FlowerPack flowerPack){
+
+    public void addFlowerPack(FlowerPack flowerPack) {
         flowerPacks.add(flowerPack);
     }
 }
